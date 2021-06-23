a864fe5c3e2a1ca81e79143f1c1eacca36668eac
benjamin-batista <79814680+benjamin-batista@users.noreply.github.com>
2021-06-23 09:45:40 -0600

    Add files via upload
    Con cambios de Khalid

---
+++ main.js
@@ -0,0 +1,29 @@
+let map
+function initMap() {
+  navigator.geolocation.getCurrentPosition(pos => {
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 8,
+      center: {lat: pos.coords.latitude, lng: pos.coords.longitude}
+    })
+  })
+}
+
+
+const socket = io()
+socket.on('busConnection', function(subject){
+  console.log(subject)
+})
+
+let busMarkers = []
+socket.on('tick', busses => {
+  busMarkers.forEach(busMarker => {
+    busMarker.setMap(null)
+  })
+  busMarkers = busses.map(bus => {
+    let marker = new google.maps.Marker({
+      position: {lat: bus.lat, lng: bus.long}
+    })
+    marker.setMap(map)
+    return marker
+  })
+})
